fb3fd008f4febdfaf28b676834c915cdf3d80755
zzzangmans1 <52357235+zzzangmans1@users.noreply.github.com>
2021-10-31 02:44:53 +0900

    update // loadstore - all_town.php

--- all_town.php
+++ all_town.php
@@ -8,8 +8,15 @@
   <body>
     <ul id="board">
       <?php
-        echo '<li class="text-left"><a id="bd_name"  href="http://http://localhost/index.php?id='.$row['id'].'"><br>'.htmlspecialchars($row['title']).'</a><br></li>'.
+      $uri= $_SERVER['REQUEST_URI'];	// 현재 uri를 구한다.
+      if($uri == "/index.php"){
+        echo '<li class="text-left"><a id="bd_name"  href="http://localhost/index.php?id='.$row['id'].'"><br>'.htmlspecialchars($row['title']).'</a><br></li>'.
         '<p class="text-right">'.htmlspecialchars($row['author'])."&nbsp;"."&nbsp;".htmlspecialchars($row['created']).'</p>'; // "&nbsp;" html 공백넣기.
+      }
+      else if($uri == "/search.php"){
+        echo '<li class="text-left"><a id="bd_name"  href="http://localhost/search.php?id='.$row['id'].'"><br>'.htmlspecialchars($row['title']).'</a><br></li>'.
+        '<p class="text-right">'.htmlspecialchars($row['author'])."&nbsp;"."&nbsp;".htmlspecialchars($row['created']).'</p>'; // "&nbsp;" html 공백넣기.
+      }
       ?>
     </ul>
   </body>
