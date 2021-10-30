af513d84ee9bfaf63f7deafc49322acbb2a7be88
zzzangmans1 <52357235+zzzangmans1@users.noreply.github.com>
2021-10-31 01:03:32 +0900

    loadstore - all_town.php

---
+++ all_town.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!-- 모든 게시글 출력 php-->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <ul id="board">
+      <?php
+        echo '<li class="text-left"><a id="bd_name"  href="http://localhost/index.php?state=Jindo_board&id='.$row['id'].'"><br>'.htmlspecialchars($row['title']).'</a><br></li>'.
+        '<p class="text-right">'.htmlspecialchars($row['author'])."&nbsp;"."&nbsp;".htmlspecialchars($row['created']).'</p>'; // "&nbsp;" html 공백넣기.
+      ?>
+    </ul>
+  </body>
+</html>
