27a941b35b4c48219e8714d4d6dba16627ca98af
zzzangmans1 <52357235+zzzangmans1@users.noreply.github.com>
2021-10-31 00:25:45 +0900

    update // loadstore - process.php

--- process.php
+++ process.php
@@ -8,9 +8,9 @@
  $conn = db_init($config["host"], $config["duser"], $config["dpw"], $config["dname"]);
  require("writeprocess.php");
  $sql = "INSERT INTO `topic` (`id`, `title`, `description` , `author` , `addr`, `addr_id`, `created`)
-                        VALUES (NULL, '".$_POST['title']."', '".$_POST['description']."', '$sign_id', '".$_POST['addr']."', '{$addr_id}', now());";
+                        VALUES (NULL, '".$_POST['title']."', '".$_POST['description']."', '{$sign_id}', '".$_POST['addr']."', '{$addr_id}', now());";
  mysqli_query($conn, $sql);
- echo "$sign_id\n님 게시물 작성 성공!!";
+ echo "$sign_id\n님 게시물 작성!!";
  echo "<meta http-equiv='refresh' content='3;url=http://localhost/index.php'>";
 
 ?>
