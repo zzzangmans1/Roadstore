9d21e2fa1c4e8a2b378cde93690b4ab7566f1f20
zzzangmans1 <52357235+zzzangmans1@users.noreply.github.com>
2021-10-31 03:50:50 +0900

    update // loadstore - town.php

--- town.php
+++ town.php
@@ -20,32 +20,32 @@
       }
     else if (strstr($_GET['where'], "Gunnae_myeon") && $row['addr_id'] == "061_02_02")  // 061_02_02 iddr_id값이 군내면
       { // 파라미터값 에 Jindo_eup 이 있구 addr_id 값이 061_544 가 맞으면 출력
-        echo '<li class="text-left"><a id="bd_name"  href="http://localhost/index.php?state=Jindo_board&id=">'.htmlspecialchars($row['title']).'</a></li>'.
+        echo '<li class="text-left"><a id="bd_name"  href="http://localhost/index.php?state=Jindo_board&id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'.
         '<p class="text-right">'.htmlspecialchars($row['author'])."&nbsp;"."&nbsp;".htmlspecialchars($row['created']).'</p>'; // "&nbsp;" html 공백넣기.
       }
     else if (strstr($_GET['where'], "GoGun_myeon") && $row['addr_id'] == "061_02_03")  // 061_02_03 iddr_id값이 고군면
     { // 파라미터값 에 Jindo_eup 이 있구 addr_id 값이 061_544 가 맞으면 출력
-        echo '<li class="text-left"><a id="bd_name"  href="http://localhost/index.php?state=Jindo_board&id=">'.htmlspecialchars($row['title']).'</a></li>'.
+        echo '<li class="text-left"><a id="bd_name"  href="http://localhost/index.php?state=Jindo_board&id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'.
         '<p class="text-right">'.htmlspecialchars($row['author'])."&nbsp;"."&nbsp;".htmlspecialchars($row['created']).'</p>'; // "&nbsp;" html 공백넣기.
       }
     else if (strstr($_GET['where'], "Uisein_myeon") && $row['addr_id'] == "061_02_04")  // 061_02_04 iddr_id값이 의신면
       { // 파라미터값 에 Jindo_eup 이 있구 addr_id 값이 061_544 가 맞으면 출력
-        echo '<li class="text-left"><a id="bd_name"  href="http://localhost/index.php?state=Jindo_board&id=">'.htmlspecialchars($row['title']).'</a></li>'.
+        echo '<li class="text-left"><a id="bd_name"  href="http://localhost/index.php?state=Jindo_board&id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'.
         '<p class="text-right">'.htmlspecialchars($row['author'])."&nbsp;"."&nbsp;".htmlspecialchars($row['created']).'</p>'; // "&nbsp;" html 공백넣기.
       }
     else if (strstr($_GET['where'], "Limhoe_myeon") && $row['addr_id'] == "061_02_05")  // 061_02_05 iddr_id값이 임회면
       { // 파라미터값 에 Jindo_eup 이 있구 addr_id 값이 061_544 가 맞으면 출력
-        echo '<li class="text-left"><a id="bd_name"  href="http://localhost/index.php?state=Jindo_board&id=">'.htmlspecialchars($row['title']).'</a></li>'.
+        echo '<li class="text-left"><a id="bd_name"  href="http://localhost/index.php?state=Jindo_board&id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'.
         '<p class="text-right">'.htmlspecialchars($row['author'])."&nbsp;"."&nbsp;".htmlspecialchars($row['created']).'</p>'; // "&nbsp;" html 공백넣기.
       }
     else if (strstr($_GET['where'], "Jisan_myeon") && $row['addr_id'] == "061_02_06")  // 061_02_06 iddr_id값이 지산면
       { // 파라미터값 에 Jindo_eup 이 있구 addr_id 값이 061_544 가 맞으면 출력
-        echo '<li class="text-left"><a id="bd_name"  href="http://localhost/index.php?state=Jindo_board&id=">'.htmlspecialchars($row['title']).'</a></li>'.
+        echo '<li class="text-left"><a id="bd_name"  href="http://localhost/index.php?state=Jindo_board&id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'.
         '<p class="text-right">'.htmlspecialchars($row['author'])."&nbsp;"."&nbsp;".htmlspecialchars($row['created']).'</p>'; // "&nbsp;" html 공백넣기.
       }
     else if (strstr($_GET['where'], "Chodo_myeon") && $row['addr_id'] == "061_02_07")  // 061_02_06 iddr_id값이 조도면
       { // 파라미터값 에 Jindo_eup 이 있구 addr_id 값이 061_544 가 맞으면 출력
-        echo '<li class="text-left"><a id="bd_name"  href="http://localhost/index.php?state=Jindo_board&id=">'.htmlspecialchars($row['title']).'</a></li>'.
+        echo '<li class="text-left"><a id="bd_name"  href="http://localhost/index.php?state=Jindo_board&id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'.
         '<p class="text-right">'.htmlspecialchars($row['author'])."&nbsp;"."&nbsp;".htmlspecialchars($row['created']).'</p>'; // "&nbsp;" html 공백넣기.
       }
 
@@ -61,22 +61,22 @@
       }
     else if (strstr($_GET['where'], "Bokgu") && $row['addr_id'] == "062_02")  // 062_02 북구
       { // 파라미터값 에 Jindo_eup 이 있구 addr_id 값이 061_544 가 맞으면 출력
-        echo '<li class="text-left"><a id="bd_name"  href="http://localhost/index.php?state=Gwangju_board&id=">'.htmlspecialchars($row['title']).'</a></li>'.
+        echo '<li class="text-left"><a id="bd_name"  href="http://localhost/index.php?state=Gwangju_board&id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'.
         '<p class="text-right">'.htmlspecialchars($row['author'])."&nbsp;"."&nbsp;".htmlspecialchars($row['created']).'</p>'; // "&nbsp;" html 공백넣기.
       }
     else if (strstr($_GET['where'], "Seogu") && $row['addr_id'] == "062_03")  // 062_03 서구
     { // 파라미터값 에 Jindo_eup 이 있구 addr_id 값이 061_544 가 맞으면 출력
-        echo '<li class="text-left"><a id="bd_name"  href="http://localhost/index.php?state=Gwangju_board&id=">'.htmlspecialchars($row['title']).'</a></li>'.
+        echo '<li class="text-left"><a id="bd_name"  href="http://localhost/index.php?state=Gwangju_board&id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'.
         '<p class="text-right">'.htmlspecialchars($row['author'])."&nbsp;"."&nbsp;".htmlspecialchars($row['created']).'</p>'; // "&nbsp;" html 공백넣기.
       }
     else if (strstr($_GET['where'], "Donggu") && $row['addr_id'] == "062_04")  // 062_04 동구
       { // 파라미터값 에 Jindo_eup 이 있구 addr_id 값이 061_544 가 맞으면 출력
-        echo '<li class="text-left"><a id="bd_name"  href="http://localhost/index.php?state=Gwangju_board&id=">'.htmlspecialchars($row['title']).'</a></li>'.
+        echo '<li class="text-left"><a id="bd_name"  href="http://localhost/index.php?state=Gwangju_board&id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'.
         '<p class="text-right">'.htmlspecialchars($row['author'])."&nbsp;"."&nbsp;".htmlspecialchars($row['created']).'</p>'; // "&nbsp;" html 공백넣기.
       }
     else if (strstr($_GET['where'], "Namgu") && $row['addr_id'] == "062_05")  // 062_05 남구
       { // 파라미터값 에 Jindo_eup 이 있구 addr_id 값이 061_544 가 맞으면 출력
-        echo '<li class="text-left"><a id="bd_name"  href="http://localhost/index.php?state=Gwangju_board&id=">'.htmlspecialchars($row['title']).'</a></li>'.
+        echo '<li class="text-left"><a id="bd_name"  href="http://localhost/index.php?state=Gwangju_board&id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'.
         '<p class="text-right">'.htmlspecialchars($row['author'])."&nbsp;"."&nbsp;".htmlspecialchars($row['created']).'</p>'; // "&nbsp;" html 공백넣기.
       }
       ?>
