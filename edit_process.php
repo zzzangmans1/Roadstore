27e6abb32378f9f34bf9b255e710c00fb53287a7
zzzangmans1 <52357235+zzzangmans1@users.noreply.github.com>
2021-12-17 00:56:56 +0900

    update - clear

--- edit_process.php
+++ edit_process.php
@@ -1,16 +1,16 @@
-<?php
-session_start();
-$sign_id = $_SESSION["user_id"]; // 세션 user_id 값을 sign_id  변수 에 넣기
-$user_name = $_SESSION['user_name'];
+ <?php
+ session_start();
+  $sign_id = $_SESSION["user_id"]; // 세션 user_id 값을 sign_id  변수 에 넣기
+  $user_name = $_SESSION['user_name'];
 
-require("config/config.php");
-require("lib/db.php");
-$conn = db_init($config["host"], $config["duser"], $config["dpw"], $config["dname"]);
-require("writeprocess.php");
-$id = mysqli_real_escape_string($conn, $_GET['id']);
-$sql = "UPDATE topic SET title={$_POST['title']}, description ={$_POST['description']}, addr = {$_POST['addr']} WHERE id =".$id;
-mysqli_query($conn, $sql);
-echo "$sign_id {$_GET['id']}\n님 게시물 수정!!";
-echo "<meta http-equiv='refresh' content='3;url=http://localhost/index.php'>";
+ require("config/config.php");
+ require("lib/db.php");
+ $conn = db_init($config["host"], $config["duser"], $config["dpw"], $config["dname"]);
+ require("writeprocess.php");
+ $sql = "INSERT INTO `topic` (`id`, `title`, `description` , `author` , `addr`, `addr_id`, `created`)
+                        VALUES (NULL, '".$_POST['title']."', '".$_POST['description']."', '{$sign_id}', '".$_POST['addr']."', '{$addr_id}', now());";
+ mysqli_query($conn, $sql);
+ echo "$sign_id\n님 게시물 작성!!";
+ echo "<meta http-equiv='refresh' content='3;url=http://localhost/index.php'>";
 
 ?>
